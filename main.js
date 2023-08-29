97c65586c3a5109db634c15fb1822dcb12c861cd
abdulloh <abdullohprogrammer@gmail.com>
2023-08-29 22:37:06 +0500

    qwerty

---
+++ main.js
@@ -0,0 +1,30 @@
+// console.log(document);
+
+// console.log(document.body.style.background = 'red');
+
+
+// console.log(document.body.style.background = 'black');
+
+
+// console.log(document.body.style.background = 'blue');
+
+
+// console.log(document.body.style.color = 'red')
+
+
+// console.log(document.body.style.color = 'blue');
+
+
+// console.log(document.body.style.color = 'white');
+
+
+// console.log(document.body.style.fontSize = '40px');
+
+
+// console.log(document.body.style.background = 'aqua');
+
+
+// console.log(document.body.style.color = 'aqua');
+
+
+// console.log(document.body.style.fontSize = '20px');
